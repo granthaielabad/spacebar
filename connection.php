fa61d2ba04a46dd16402d3ccf950aeb878c03988
MumuNiMochii <69304294+MumuNiMochii@users.noreply.github.com>
2020-09-28 20:55:01 +0800

    Add files via upload
    New updates include: "link rel='icon'", "$_POST and $_GET methods", "JQuery", "imports", etc.

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 	//establish connection to the server and db
-	$dbServerName = "localhost";
-	$dbUsername = "root";
-	$dbPassword = "";
-	$dbName = "nfposts";
+	$dbServerName = "sql208.epizy.com";
+	$dbUsername = "epiz_26810500";
+	$dbPassword = "rQz6dbux0sLrkz";
+	$dbName = "epiz_26810500_nfposts";
 	
 	$connection = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
 ?>
--- nfgetpost.php
+++ nfgetpost.php
@@ -3,20 +3,43 @@
 
 	$nfpostNewCount = $_POST['nfpostNewCount'];
 	
-	$sql = "SELECT * FROM news LIMIT $nfpostNewCount";
+	$sql = "SELECT * FROM news";
 	$result = mysqli_query($connection, $sql);
 	if (mysqli_num_rows($result) > 0) {
 		while ($row = mysqli_fetch_assoc($result)) {
-			echo "<p>";
-			echo "<br>";
-			echo "ID: ".$row['id'];
-			echo "<br>";
-			echo "Subject: ".$row['subject'];
-			echo "<br>";
-			echo "Story: ".$row['story'];
-			echo "<br>";
-			echo "Tags: ".$row['tags'];
-			echo "</p>";
+			echo "<style>
+			* {
+				margin: 0;
+				padding: 0;
+			}
+			#mainTable {
+				margin: 20px 20% 20px 20%;
+				width: 60%;
+				background-color: #1a1a1a;
+				border: 1px solid transparent;
+				border-radius: 6px;
+                display: grid;
+			}
+			#mainTable:hover {
+				background-color: #262626;
+			}
+            #divbase {
+                display: grid;
+                grid-template-columns: auto;
+                grid-template-rows: auto auto auto;
+            }
+			</style>";
+			echo "<table id='mainTable'>
+				    <tr>
+                        <td>
+                            <div id='divbase'>
+                                            <div id='R1_C1'><div id='R1_C1_1'>".$row['id']."</div><div id='R1_C2_1'>".$row['subject']."</div></div>
+                                            <div id='R2_C1'>".$row['story']."</div>
+                                            <div id='R2_C1'>".$row['tags' ]."</div>
+                            </div>
+                        </td>
+                    </tr>
+			    </table>";
 		}
 	} else {
 		echo "There are no posts.";
